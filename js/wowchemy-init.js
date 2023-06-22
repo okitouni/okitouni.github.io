97b0260648e32b88060b0eec4d4043abc7fc4659
okitouni <okitouni@users.noreply.github.com>
2023-06-22 20:57:54 +0000

    deploy: 35720874288ce1199b6e1226f7ade3f682981898

--- js/wowchemy-init.js
+++ js/wowchemy-init.js
@@ -1,5 +1,5 @@
 (() => {
-  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20201208221717-1d5456668842/assets/js/wowchemy-theming.js
+  // ns-hugo:/tmp/hugo_cache_runner/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20201208221717-1d5456668842/assets/js/wowchemy-theming.js
   function getThemeMode() {
     return parseInt(localStorage.getItem("wcTheme") || 2);
   }
